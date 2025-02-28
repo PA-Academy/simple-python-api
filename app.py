dfb689dc22b9737ddf7fb60ba3e242ff07d24870
Barry Abrahamson <barry.abrahamson@gmail.com>
2025-02-28 16:07:58 +0000

    Resolved vulnerability code

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ def hello():
 
 @app.route('/api/user/<name>', methods=['GET'])
 def user(name):
-    return f"Welcome, {name}"  # Potential XSS vulnerability
+    return f"Welcome, {escape(name)}"  # Potential XSS vulnerability
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
